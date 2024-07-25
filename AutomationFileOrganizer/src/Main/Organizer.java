4936928836bdc70d393c9c8ddbc080d0b22fef1e
VSD1995 <vishaldaberaooffice@gmail.com>
2024-07-25 18:12:51 +0530

    PH2 Complete the Project

--- AutomationFileOrganizer/src/Main/Organizer.java
+++ AutomationFileOrganizer/src/Main/Organizer.java
@@ -1,6 +1,10 @@
 package Main;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class Organizer  {
 	
@@ -21,7 +25,9 @@ public class Organizer  {
 			if(file.isFile()) {
 			String fileName = file.getName();
 			String fileExtension = getFileExtension(fileName);
-			System.out.println(fileName);
+			if(!fileExtension.isEmpty()) {
+				moveToDirectory(file, fileExtension);
+			}
 			}
 		}
 	}
@@ -32,5 +38,23 @@ public class Organizer  {
 			return"";
 		}
 		return fileName.substring(lastIndexDot+1).toLowerCase();
+	}
+	
+	private void moveToDirectory(File file, String directoryName) {
+		Path sourcePath = file.toPath();
+		Path targetDirectroy = sourcePath.getParent().resolve(directoryName);
+		
+		try {
+			if(!Files.exists(targetDirectroy)) {
+				Files.createDirectories(targetDirectroy);
+			}
+			
+			Path targetPath = targetDirectroy.resolve(file.getName());
+			Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+			System.out.println("Move file : "+file.getName()+" to "+directoryName+" Directory.");
+		} catch (IOException e) {
+			System.err.println("Error moving file :"+ file.getName());
+			e.printStackTrace();
+		}
 	}
 }
